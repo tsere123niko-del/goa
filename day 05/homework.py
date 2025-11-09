9814e4003cd2ee9e7f7b65d12e92e5f66d47c548
tsere123niko-del <tsere123niko@gmail.com>
2025-11-09 21:20:11 +0400

    Add files via upload

---
+++ day 05/homework.py
@@ -0,0 +1,23 @@
+# debugging ნიშნავს იმას რომ რაიმე შეცდომები გავასწოროთ
+
+Name=("age 14")
+print(Name)
+
+Num1=(4564)
+Num2=(4)
+print(Num1 / Num2)
+
+Num3=(9768)
+Num4=(3)
+print(Num3/Num4)
+
+
+
+
+
+name="saba"
+name="giorgi"
+print(name)
+
+num=40
+print(num*3)
