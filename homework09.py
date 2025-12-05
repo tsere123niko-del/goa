374b48a6f2042df07a1c5cdf0a47356b3fae23c4
tsere123niko-del <tsere123niko@gmail.com>
2025-12-05 18:13:24 +0400

    Add files via upload

---
+++ homework09.py
@@ -0,0 +1,15 @@
+# str() ფუნქცია რაღაცა რიცხვს ან სიტყვას აქცევს ინტეჯერად int() გადაყავს ინტეჯერად float() გადაყავს წილად რიცხვად
+
+დედა = int(input("enter mom age: "))
+მამა = int(input("enter dad age: "))
+ბებია = int(input("enter grandmother age: "))
+
+print(დედა + 25)
+print(მამა + 25)
+print(ბებია + 25)
+
+name = type(input("ჩაწერეთ თქვენი სახელი: "))
+print(name)
+
+age = int(input("ჩაწერეთ თქვენი სახელი: "))
+print(age)
