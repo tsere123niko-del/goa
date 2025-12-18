883e1f269e5c3ba4ac10dcaed5a19b082fbddf75
tsere123niko-del <tsere123niko@gmail.com>
2025-12-18 18:17:08 +0400

    Add files via upload

---
+++ homework11.py
@@ -0,0 +1,14 @@
+or1 = True or False
+or2 = False or True
+or3 = True or True
+or4 = False or False
+
+and1 = True and False
+and2 = False and True
+and3 = True and True
+and4 = False and False
+
+num1 = int(input("შეიყვანე num1: "))
+num2 = int(input("შეიყვანე num2: "))
+
+print(num1 % num2)
