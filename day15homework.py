2581bb9f5656f9cc40937b311f2cb5b0b197a6b5
tsere123niko-del <tsere123niko@gmail.com>
2026-01-16 13:50:03 +0400

    Add files via upload

---
+++ day15homework.py
@@ -0,0 +1,26 @@
+name = input("enter your name: ")
+
+if name == "nikolozi" :
+    print("Hello")
+else:
+    print("Bye")
+
+
+my_favoritenumber = 91
+user_favoritenumber = int(input("enetr favorite number: "))
+
+if user_favoritenumber == my_favoritenumber:
+    print("Perfect")
+elif user_favoritenumber > my_favoritenumber:
+    print("More")
+else:
+    print("Less")
+
+
+user_name = input("შეიყვანე შენი სახელი: ")
+user_age = int(input("შეიყვანე შენი ასაკი: "))
+
+if user_name == "nikolozi" and user_age == "14":
+    print("Twins")
+else:
+    print("Not Twins")
